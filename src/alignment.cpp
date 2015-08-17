aee460461bcaf801f776efdfe4474126233b9789
Mario Kostelac <mario.kostelac@gmail.com>
2015-08-17 11:56:32 +0200

    add banded alignment
    - added banded alignment
    - wrapper library into POA namespace

--- src/alignment.cpp
+++ src/alignment.cpp
@@ -26,202 +26,251 @@ using std::string;
 using std::tuple;
 using std::vector;
 
-int Alignment::match_score_ = 4;
-int Alignment::mismatch_score_ = -2;
-int Alignment::open_gap_score_ = -4;
-int Alignment::extend_gap_score_ = -2;
+namespace POA {
+
+    int Alignment::match_score_ = 4;
+    int Alignment::mismatch_score_ = -2;
+    int Alignment::open_gap_score_ = -4;
+    int Alignment::extend_gap_score_ = -2;
 
 
-Alignment::Alignment(const string& sequence,
-                     const Graph& graph): sequence_(sequence),
-                                          graph_(graph) {
-    assert(sequence.size() > 0);
-}
-
-/**
- * @brief returns move score
- * @details returns move score
- *
- * @param move move
- * @return move score
- */
-int move_score(const move& move) {
-    return get<0>(move);
-}
-
-
-void Alignment::backtrack() {
-    while (scores_[max_i_][max_j_] > 0 && !(max_i_ == 0 && max_j_ == 0)) {
-        int next_i = backtrack_graph_idx_[max_i_][max_j_];
-        int next_j = backtrack_seq_idx_[max_i_][max_j_];
-        uint32_t seq_idx = max_j_ - 1;
-        uint32_t node_id = index_to_nodeID_[max_i_ - 1];
-
-        seq_idxs_.emplace_front(next_j != max_j_ ? seq_idx : -1);
-        node_ids_.emplace_front(next_i != max_i_ ? node_id : -1);
-
-        max_i_ = next_i;
-        max_j_ = next_j;
+    Alignment::Alignment(const string& sequence,
+                         const Graph& graph): sequence_(sequence),
+                                              graph_(graph) {
+        assert(sequence.size() > 0);
     }
-}
+
+    /**
+     * @brief returns move score
+     * @details returns move score
+     *
+     * @param move move
+     * @return move score
+     */
+    int move_score(const move& move) {
+        return get<0>(move);
+    }
 
 
-void Alignment::align() {
-    align_starting_at(0);
-}
+    /**
+     * @brief returns move graph_idx
+     * @details returns move graph_idx
+     *
+     * @param move move
+     * @return move graph_index
+     */
+    int move_graph_idx(const move& move) {
+        return get<1>(move);
+    }
 
-void Alignment::align_starting_at(const uint32_t min_pos) {
-    uint32_t m = sequence_.length();
-    max_score_ = numeric_limits<int>::min();
-    max_i_ = -1;
-    max_j_ = -1;
 
-    init_dp_tables(min_pos);
+    /**
+     * @brief returns move seq_idx
+     * @details returns move seq_idx
+     *
+     * @param move move
+     * @return move seq_idx
+     */
+    int move_seq_idx(const move& move) {
+        return get<2>(move);
+    }
 
-    vector<int> insert_cost((valid_nodes_num_+1)*(m+1), open_gap_score_);
-    vector<int> delete_cost((valid_nodes_num_+1)*(m+1), open_gap_score_);
 
-    auto match_score = [](char seq_base, char node_base) -> int {
-        if (seq_base == node_base) {
-            return match_score_;
-        } else {
-            return mismatch_score_;
+    /**
+     * @brief returns move type
+     * @details returns move type
+     *
+     * @param move move
+     * @return move type
+     */
+    char move_type(const move& move) {
+        return get<3>(move);
+    }
+
+
+    void Alignment::backtrack() {
+        while (scores_[max_i_][max_j_] > 0 && !(max_i_ == 0 && max_j_ == 0)) {
+            int next_i = backtrack_graph_idx_[max_i_][max_j_];
+            int next_j = backtrack_seq_idx_[max_i_][max_j_];
+            uint32_t seq_idx = max_j_ - 1;
+            uint32_t node_id = index_to_nodeID_[max_i_ - 1];
+
+            seq_idxs_.emplace_front(next_j != max_j_ ? seq_idx : -1);
+            node_ids_.emplace_front(next_i != max_i_ ? node_id : -1);
+
+            max_i_ = next_i;
+            max_j_ = next_j;
         }
-    };
+    }
 
-    // modified smith-waterman(SW)
-    // operation candidates
-    // char values:
-    // -> 'I' - insertion
-    // -> 'D' - deletion
-    // -> 'M' - match or mismatch
-    for (uint32_t i = 0; i < valid_nodes_num_; ++i) {
-        auto& node = graph_.getNode(index_to_nodeID_[i]);
-        const char base = node->base();
 
-        for (uint32_t j = 0; j < sequence_.length(); ++j) {
-            // insertion from sequence, unchanged as in SW
-            move best_candidate(scores_[i + 1][j] + insert_cost[(i + 1)*m + j],
-                                    i + 1, j, 'I');
+    void Alignment::align() {
+        align_banded_starting_at(0, -1);
+    }
 
-            if (node->getPredecessorsIds().size()) {
-              // for every other operation I have to check for all predeccesors of
-              // current node
-              for (auto node_id : node->getPredecessorsIds()) {
-                auto prev_index = index_from_node_id(node_id);
+    void Alignment::align_starting_at(const uint32_t pos) {
+        align_banded_starting_at(pos, -1);
+    }
 
-                // match/mismatch
-                move mm(scores_[prev_index + 1][j] +
-                    match_score(sequence_[j], base),
-                    prev_index + 1, j, 'M');
-                if (move_score(mm) >= move_score(best_candidate)) {
-                  best_candidate = mm;
-                }
+    void Alignment::align_banded_starting_at(const uint32_t min_pos, const int band_width) {
+        uint32_t m = sequence_.length();
+        max_score_ = numeric_limits<int>::min();
+        max_i_ = -1;
+        max_j_ = -1;
 
-                // insertion from graph to sequence / deletion
-                move ins(scores_[prev_index + 1][j + 1] +
-                    delete_cost[(prev_index + 1)*m + j + 1],
-                    prev_index + 1, j + 1, 'D');
-                if (move_score(ins) >= move_score(best_candidate)) {
-                  best_candidate = ins;
-                }
-              }
+        init_dp_tables(min_pos);
+
+        vector<int> insert_cost((valid_nodes_num_+1)*(m+1), open_gap_score_);
+        vector<int> delete_cost((valid_nodes_num_+1)*(m+1), open_gap_score_);
+
+        auto match_score = [](char seq_base, char node_base) -> int {
+            if (seq_base == node_base) {
+                return match_score_;
             } else {
-              int prev_index = -1;
-              // match/mismatch
-              move mm(scores_[prev_index + 1][j] +
-                  match_score(sequence_[j], base),
-                  prev_index + 1, j, 'M');
-              if (move_score(mm) >= move_score(best_candidate)) {
-                best_candidate = mm;
-              }
+                return mismatch_score_;
+            }
+        };
 
-              // insertion from graph to sequence / deletion
-              move ins(scores_[prev_index + 1][j + 1] +
-                  delete_cost[(prev_index + 1)*m + j + 1],
-                  prev_index + 1, j + 1, 'D');
-              if (move_score(ins) >= move_score(best_candidate)) {
-                best_candidate = ins;
-              }
+        // modified smith-waterman(SW)
+        // operation candidates
+        // char values:
+        // -> 'I' - insertion
+        // -> 'D' - deletion
+        // -> 'M' - match or mismatch
+        for (uint32_t i = 0; i < valid_nodes_num_; ++i) {
+            const auto node_id = index_to_nodeID_[i];
+            auto& node = graph_.getNode(node_id);
+            const char base = node->base();
+
+            // calculate sequence substring that should be part of DP
+            int lo = 0, hi = sequence_.length();
+            if (band_width >= 0) {
+                lo = max(0, (int) (graph_.min_node_distance(node_id) - band_width - min_pos));
+                hi = min((int) sequence_.length(), (int) (graph_.max_node_distance(node_id) + band_width - min_pos + 1));
             }
 
-            // update dp and backtrack tables
-            scores_[i + 1][j + 1] = get<0>(best_candidate);
-            backtrack_graph_idx_[i + 1][j + 1] = get<1>(best_candidate);
-            backtrack_seq_idx_[i + 1][j + 1] = get<2>(best_candidate);
+            for (uint32_t j = lo; j < hi; ++j) {
+                // insertion from sequence, unchanged as in SW
+                move best_candidate(scores_[i + 1][j] + insert_cost[(i + 1)*m + j],
+                                        i + 1, j, 'I');
 
-            char move = get<3>(best_candidate);
-            if (move == 'I') {
-                insert_cost[(i + 1) * m + (j + 1)] = extend_gap_score_;
-            } else if (move == 'D') {
-                delete_cost[(i + 1) * m + (j + 1)] = extend_gap_score_;
-            }
+                if (node->getPredecessorsIds().size()) {
+                  // for every other operation I have to check for all predeccesors of
+                  // current node
+                  for (auto node_id : node->getPredecessorsIds()) {
+                    auto prev_index = index_from_node_id(node_id);
 
-            // because of local alignment minimum score should be zero
-            if (scores_[i + 1][j + 1] < 0) {
-                scores_[i + 1][j + 1] = 0;
-                backtrack_graph_idx_[i + 1][j + 1] = -1;
-                backtrack_seq_idx_[i + 1][j + 1] = -1;
-            }
+                    // match/mismatch
+                    move mm(scores_[prev_index + 1][j] +
+                        match_score(sequence_[j], base),
+                        prev_index + 1, j, 'M');
+                    if (move_score(mm) >= move_score(best_candidate)) {
+                      best_candidate = mm;
+                    }
+
+                    // insertion from graph to sequence / deletion
+                    move ins(scores_[prev_index + 1][j + 1] +
+                        delete_cost[(prev_index + 1)*m + j + 1],
+                        prev_index + 1, j + 1, 'D');
+                    if (move_score(ins) >= move_score(best_candidate)) {
+                      best_candidate = ins;
+                    }
+                  }
+                } else {
+                  int prev_index = -1;
+                  // match/mismatch
+                  move mm(scores_[prev_index + 1][j] +
+                      match_score(sequence_[j], base),
+                      prev_index + 1, j, 'M');
+                  if (move_score(mm) >= move_score(best_candidate)) {
+                    best_candidate = mm;
+                  }
+
+                  // insertion from graph to sequence / deletion
+                  move ins(scores_[prev_index + 1][j + 1] +
+                      delete_cost[(prev_index + 1)*m + j + 1],
+                      prev_index + 1, j + 1, 'D');
+                  if (move_score(ins) >= move_score(best_candidate)) {
+                    best_candidate = ins;
+                  }
+                }
+
+                // update dp and backtrack tables
+                scores_[i + 1][j + 1] = move_score(best_candidate);
+                backtrack_graph_idx_[i + 1][j + 1] = move_graph_idx(best_candidate);
+                backtrack_seq_idx_[i + 1][j + 1] = move_seq_idx(best_candidate);
+
+                char move = move_type(best_candidate);
+                if (move == 'I') {
+                    insert_cost[(i + 1) * m + (j + 1)] = extend_gap_score_;
+                } else if (move == 'D') {
+                    delete_cost[(i + 1) * m + (j + 1)] = extend_gap_score_;
+                }
+
+                // because of local alignment minimum score should be zero
+                if (scores_[i + 1][j + 1] < 0) {
+                    scores_[i + 1][j + 1] = 0;
+                    backtrack_graph_idx_[i + 1][j + 1] = -1;
+                    backtrack_seq_idx_[i + 1][j + 1] = -1;
+                }
 
 
-            // update max score and its position
-            if (scores_[i + 1][j + 1] >= max_score_) {
-                max_i_ = i + 1;
-                max_j_ = j + 1;
-                max_score_ = scores_[i + 1][j + 1];
+                // update max score and its position
+                if (scores_[i + 1][j + 1] >= max_score_) {
+                    max_i_ = i + 1;
+                    max_j_ = j + 1;
+                    max_score_ = scores_[i + 1][j + 1];
+                }
             }
         }
+
+        backtrack();
     }
 
-    backtrack();
-}
 
+    void Alignment::init_dp_tables(const int min_pos) {
+        uint32_t m = sequence_.length();
+        valid_nodes_num_ = 0;
 
-void Alignment::init_dp_tables(const int min_pos) {
-    uint32_t m = sequence_.length();
-    valid_nodes_num_ = 0;
+        max_valid_node_id_ = numeric_limits<int>::min();
+        min_valid_node_id_ = numeric_limits<int>::max();
 
-    max_valid_node_id_ = numeric_limits<int>::min();
-    min_valid_node_id_ = numeric_limits<int>::max();
-
-    const vector<uint32_t>& nodes_ids = const_cast<Graph&>(graph_).getNodesIds();
-    for (int node_id: nodes_ids) {
-      if (graph_.node_distance(node_id) >= min_pos) {
-        valid_nodes_num_++;
-        max_valid_node_id_ = max(max_valid_node_id_, node_id);
-        min_valid_node_id_ = min(min_valid_node_id_, node_id);
-      }
-    }
-
-    fprintf(stderr, "%d %d\n", max_valid_node_id_, min_valid_node_id_);
-
-    index_to_nodeID_.resize(valid_nodes_num_);
-    nodeID_to_index_.resize(max_valid_node_id_ - min_valid_node_id_ + 1, -1);
-
-    uint32_t idx = 0;
-    for (int node_id: nodes_ids) {
-        if (graph_.node_distance(node_id) < min_pos) {
-            continue;
+        const vector<uint32_t>& nodes_ids = const_cast<Graph&>(graph_).getNodesIds();
+        for (int node_id: nodes_ids) {
+          if (graph_.max_node_distance(node_id) >= min_pos) {
+            valid_nodes_num_++;
+            max_valid_node_id_ = max(max_valid_node_id_, node_id);
+            min_valid_node_id_ = min(min_valid_node_id_, node_id);
+          }
         }
 
-        nodeID_to_index_[node_id - min_valid_node_id_] = idx;
-        index_to_nodeID_[idx] = node_id;
-        idx++;
+        index_to_nodeID_.resize(valid_nodes_num_);
+        nodeID_to_index_.resize(max_valid_node_id_ - min_valid_node_id_ + 1, -1);
+
+        uint32_t idx = 0;
+        for (int node_id: nodes_ids) {
+            if (graph_.max_node_distance(node_id) < min_pos) {
+                continue;
+            }
+
+            nodeID_to_index_[node_id - min_valid_node_id_] = idx;
+            index_to_nodeID_[idx] = node_id;
+            idx++;
+        }
+
+        // init dynamic programming smith waterman matrix
+        scores_.resize(valid_nodes_num_ + 1, vector<int>(m + 1, 0));
+
+
+        // init backtracking matrices
+        backtrack_seq_idx_.resize(valid_nodes_num_ + 1, vector<int>(m + 1, -1));
+        backtrack_graph_idx_.resize(valid_nodes_num_ + 1, vector<int>(m + 1, -1));
     }
 
-    // init dynamic programming smith waterman matrix
-    scores_.resize(valid_nodes_num_ + 1, vector<int>(m + 1, 0));
-
-
-    // init backtracking matrices
-    backtrack_seq_idx_.resize(valid_nodes_num_ + 1, vector<int>(m + 1, 0));
-    backtrack_graph_idx_.resize(valid_nodes_num_ + 1, vector<int>(m + 1, 0));
-}
-
-int Alignment::index_from_node_id(const uint32_t node_id) const {
-    if (node_id < min_valid_node_id_ || node_id > max_valid_node_id_) {
-        return -1;
+    int Alignment::index_from_node_id(const uint32_t node_id) const {
+        if (node_id < min_valid_node_id_ || node_id > max_valid_node_id_) {
+            return -1;
+        }
+        return nodeID_to_index_[(int) node_id - min_valid_node_id_];
     }
-    return nodeID_to_index_[(int) node_id - min_valid_node_id_];
 }
--- src/alignment.hpp
+++ src/alignment.hpp
@@ -24,166 +24,192 @@ using std::tuple;
 using std::deque;
 using std::unordered_map;
 
+namespace POA {
 
-typedef tuple<int, int, int, char> move;
+    typedef tuple<int, int, int, char> move;
 
-/**
- * @brief Alignment class
- * @details Calculates local alignment between graph nodes and sequence
- * using modified smith-waterman algorithm.
- * 
- */
-class Alignment {
- public:
     /**
-     * @brief Alignment constructor
-     * @details Constructor needs two parameters - sequence and graph
-     * for calculating alignment between those two.
+     * @brief Alignment class
+     * @details Calculates local alignment between graph nodes and sequence
+     * using modified smith-waterman algorithm.
      * 
-     * @param sequence reference to string sequence
-     * @param graph reference to graph
      */
-    Alignment(const string& sequence, const Graph& graph);
+    class Alignment {
+     public:
+        /**
+         * @brief Alignment constructor
+         * @details Constructor needs two parameters - sequence and graph
+         * for calculating alignment between those two.
+         * 
+         * @param sequence reference to string sequence
+         * @param graph reference to graph
+         */
+        Alignment(const string& sequence, const Graph& graph);
 
 
-    /**
-     * @brief Aligns sequence to graph
-     * @details Method performs local alignment between
-     * string sequence and directed acyclic graph (DAG).
-     * Algorithm is variation of smith-waterman algorithm 
-     * (http://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm).
-     */
-    void align();
+        /**
+         * @brief Aligns sequence to graph
+         * @details Method performs local alignment between
+         * string sequence and directed acyclic graph (DAG).
+         * Algorithm is variation of smith-waterman algorithm 
+         * (http://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm).
+         */
+        void align();
 
 
-    /**
-     * @brief Aligns sequence to part of graph which represents part of sequence
-     * on position >= pos.
-     * @details Method is created to improve efficieny while aligning sequence
-     * that should occur in some later parts of the graph.
-     * Example:
-     * AAAAAAAAAAABBBBBBFBBCCCCDC (seq1)
-     *                  BBBCCCDCC (seq2)
-     * seq1.indexOf('F') - THRESHOLD would be a nice 'pos' to pass since more than
-     * a half of string won't be part od DP.
-     *
-     * Method performs local alignment between
-     * string sequence and directed acyclic graph (DAG).
-     * Algorithm is variation of smith-waterman algorithm
-     * (http://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm).
-     * @param pos minimum position where to align
-     */
-    void align_starting_at(const uint32_t pos);
+        /**
+         * @brief Aligns sequence to part of graph which represents part of sequence
+         * on position >= pos.
+         * @details Method is created to improve efficieny while aligning sequence
+         * that should occur in some later parts of the graph.
+         * Example:
+         * AAAAAAAAAAABBBBBBFBBCCCCDC (seq1)
+         *                  BBBCCCDCC (seq2)
+         * seq1.indexOf('F') - THRESHOLD would be a nice 'pos' to pass since more than
+         * a half of string won't be part od DP.
+         *
+         * Method performs local alignment between
+         * string sequence and directed acyclic graph (DAG).
+         * Algorithm is variation of smith-waterman algorithm
+         * (http://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm).
+         * @param pos minimum position where to align
+         */
+        void align_starting_at(const uint32_t pos);
 
 
-    /**
-     * @brief Getter for sequence
-     * @return sequence
-     */
-    const string& sequence() const { return sequence_; }
+        /**
+         * @brief Aligns sequence to part of graph which represents part of sequence
+         * on position >= pos, with given band width.
+         * @details Method is created to improve efficieny while aligning sequence
+         * that should occur in some later parts of the graph.
+         * Example:
+         * AAAAAAAAAAABBBBBBFBBCCCCDC (seq1)
+         *                  BBBCCCDCC (seq2)
+         * seq1.indexOf('F') - THRESHOLD would be a nice 'pos' to pass since more than
+         * a half of string won't be part od DP.
+         *
+         * Banded alignment explained in comment on https://www.biostars.org/p/2071/.
+         * Essentialy, just the "diagonal" of 2*band_width will be considered for DP.
+         *
+         * Method performs local alignment between
+         * string sequence and directed acyclic graph (DAG).
+         * Algorithm is variation of smith-waterman algorithm
+         * (http://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm).
+         * @param pos minimum position where to align
+         * @param band_width the band_width, -1 for no limit
+         */
+        void align_banded_starting_at(const uint32_t pos, const int band_width);
 
 
-    /**
-     * @brief Getter for sequence indices in alignment
-     * @details Method returns sequence indices representing
-     * alignment. Index value is -1 if node in graph isn't aligned
-     * to any position/character in sequence, otherwise it is position
-     * in sequence string.
-     * @return sequence indices
-     */
-    const deque<int>& seq_idxs() const { return seq_idxs_; }
+        /**
+         * @brief Getter for sequence
+         * @return sequence
+         */
+        const string& sequence() const { return sequence_; }
 
 
-    /**
-     * @brief Getter for node ids in alignment
-     * @details Methor returns node ids representing
-     * alignment. Id value is -1 if some character in sequence
-     * isn't aligned to any node in graph, otherwise it is id of node
-     * in graph.
-     * @return node ids
-     */
-    const deque<int>& node_ids() const { return node_ids_; }
+        /**
+         * @brief Getter for sequence indices in alignment
+         * @details Method returns sequence indices representing
+         * alignment. Index value is -1 if node in graph isn't aligned
+         * to any position/character in sequence, otherwise it is position
+         * in sequence string.
+         * @return sequence indices
+         */
+        const deque<int>& seq_idxs() const { return seq_idxs_; }
 
 
- private:
-    // value of match in smith-waterman score matrix
-    static int match_score_;
-    // value of mismatch in smith-waterman score matrix
-    static int mismatch_score_;
-    // value of gap opening in smith-waterman score matrix
-    static int open_gap_score_;
-    // value of extending gap in smith-waterman score matrix
-    static int extend_gap_score_;
-
-    // sequence to be aligned to graph
-    const string& sequence_;
-    // poa graph
-    const Graph& graph_;
-
-    // smith-waterman dynamic programming score matrix
-    vector<vector<int>> scores_;
-    // backtracking matrix for sequence
-    vector<vector<int>> backtrack_seq_idx_;
-    // backtracking matrix for graph
-    vector<vector<int>> backtrack_graph_idx_;
-
-    // sequence indices representing alignment
-    deque<int> seq_idxs_;
-    // node ids representing alignment
-    deque<int> node_ids_;
-
-    // map node ID to index in dp matrix
-    vector<int> nodeID_to_index_;
-    // map index in dp matrix to node ID
-    vector<uint32_t> index_to_nodeID_;
-
-    // max score in matrix
-    int max_score_;
-    // row index of maximum score
-    int max_i_;
-    // column index of maximum score
-    int max_j_;
-
-    // number of graph nodes that could be aligned with the sequence.
-    // All nodes that occur before given start position for sure will not be accounted here.
-    int valid_nodes_num_;
-
-    // minimal valid node_id
-    // used for compressing nodeID_to_index_ field
-    int max_valid_node_id_;
-
-    // maximal valid node_id
-    // used for compressing nodeID_to_index_ field
-    int min_valid_node_id_;
+        /**
+         * @brief Getter for node ids in alignment
+         * @details Methor returns node ids representing
+         * alignment. Id value is -1 if some character in sequence
+         * isn't aligned to any node in graph, otherwise it is id of node
+         * in graph.
+         * @return node ids
+         */
+        const deque<int>& node_ids() const { return node_ids_; }
 
 
-    /**
-     * @brief Returns index of given node_id in DP table. If node is
-     * not part of DP, returns -1.
-     *
-     * @param node_id node id
-     * @return index in DP table if exists, otherwise -1.
-     */
-    int index_from_node_id(const uint32_t node_id) const;
+     private:
+        // value of match in smith-waterman score matrix
+        static int match_score_;
+        // value of mismatch in smith-waterman score matrix
+        static int mismatch_score_;
+        // value of gap opening in smith-waterman score matrix
+        static int open_gap_score_;
+        // value of extending gap in smith-waterman score matrix
+        static int extend_gap_score_;
+
+        // sequence to be aligned to graph
+        const string& sequence_;
+        // poa graph
+        const Graph& graph_;
+
+        // smith-waterman dynamic programming score matrix
+        vector<vector<int>> scores_;
+        // backtracking matrix for sequence
+        vector<vector<int>> backtrack_seq_idx_;
+        // backtracking matrix for graph
+        vector<vector<int>> backtrack_graph_idx_;
+
+        // sequence indices representing alignment
+        deque<int> seq_idxs_;
+        // node ids representing alignment
+        deque<int> node_ids_;
+
+        // map node ID to index in dp matrix
+        vector<int> nodeID_to_index_;
+        // map index in dp matrix to node ID
+        vector<uint32_t> index_to_nodeID_;
+
+        // max score in matrix
+        int max_score_;
+        // row index of maximum score
+        int max_i_;
+        // column index of maximum score
+        int max_j_;
+
+        // number of graph nodes that could be aligned with the sequence.
+        // All nodes that occur before given start position for sure will not be accounted here.
+        int valid_nodes_num_;
+
+        // minimal valid node_id
+        // used for compressing nodeID_to_index_ field
+        int max_valid_node_id_;
+
+        // maximal valid node_id
+        // used for compressing nodeID_to_index_ field
+        int min_valid_node_id_;
 
 
-    /**
-     * @brief Initializes matrices for smith-waterman
-     * @details Initializes dynamic programming score
-     * matrix, and backtracking matrices for keeping
-     * track of best alignment path.
-     * Since some nodes are out of DP (if aligning starts from pos > 0),
-     * matrix is "sparse".
-     * @param starting_pos minimum starting position in result sequence
-     * where the query sequence is going to be aligned.
-     */
-    void init_dp_tables(const int starting_pos);
+        /**
+         * @brief Returns index of given node_id in DP table. If node is
+         * not part of DP, returns -1.
+         *
+         * @param node_id node id
+         * @return index in DP table if exists, otherwise -1.
+         */
+        int index_from_node_id(const uint32_t node_id) const;
 
 
-    /**
-     * @brief Backtracks best alignment path
-     */
-    void backtrack();
-};
+        /**
+         * @brief Initializes matrices for smith-waterman
+         * @details Initializes dynamic programming score
+         * matrix, and backtracking matrices for keeping
+         * track of best alignment path.
+         * Since some nodes are out of DP (if aligning starts from pos > 0),
+         * matrix is "sparse".
+         * @param starting_pos minimum starting position in result sequence
+         * where the query sequence is going to be aligned.
+         */
+        void init_dp_tables(const int starting_pos);
+
+
+        /**
+         * @brief Backtracks best alignment path
+         */
+        void backtrack();
+    };
+}
 
 #endif  // ALIGNMENT_H
